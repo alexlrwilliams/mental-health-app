759350366e247a08d823d27a8fe86b793c1db37f
Alex Williams <aw01121@surrey.ac.uk>
2023-03-09 22:58:10 +0100

    Setup microservice repos

---
+++ appointment-service/src/test/java/com/com3014/appointmentservice/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com3014.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
