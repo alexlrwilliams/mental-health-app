92ee6208790c999388d6a95487644474737ecd43
Alex Williams <aw01121@surrey.ac.uk>
2023-03-11 19:11:43 +0100

    added main.yml

--- appointment-service/src/test/java/com/com3014/appointmentservice/AppointmentServiceApplicationTests.java
+++ appointment-service/src/test/java/com/com3014/appointmentservice/AppointmentServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.com3014.appointmentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
 
 @SpringBootTest
 class AppointmentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(2);
 	}
 
 }
