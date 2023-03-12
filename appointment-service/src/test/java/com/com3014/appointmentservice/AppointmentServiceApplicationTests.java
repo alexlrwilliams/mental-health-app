5c2e158ab49b66dad5ad7eb33c131dbe57ad5668
Alex Williams <aw01121@surrey.ac.uk>
2023-03-12 00:14:00 +0000

    test failed build for PR

--- appointment-service/src/test/java/com/com3014/appointmentservice/AppointmentServiceApplicationTests.java
+++ appointment-service/src/test/java/com/com3014/appointmentservice/AppointmentServiceApplicationTests.java
@@ -3,12 +3,14 @@ package com.com3014.appointmentservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AppointmentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(2);
 	}
 
 }
