443f52c16bc385dea7479df49b7550f9d77fd980
Alex Williams <aw01121@surrey.ac.uk>
2023-03-22 14:56:28 +0000

    MHC-17: Addded route tests

---
+++ api-gateway/src/test/java/com/com3014/apigateway/SpringGatewayAppointmentServiceRoutingTest.java
@@ -0,0 +1,58 @@
+package com.com3014.apigateway;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWireMock(port = 8083)
+public class SpringGatewayAppointmentServiceRoutingTest {
+
+    @LocalServerPort
+    protected int port = 0;
+
+    protected WebTestClient webClient;
+
+    @BeforeEach
+    public void setup() {
+        this.webClient = WebTestClient
+                .bindToServer()
+                .responseTimeout(Duration.ofSeconds(10))
+                .baseUrl("http://localhost:" + port)
+                .build();
+
+        stubFor(
+                get(urlMatching("/api/appointments.*")).willReturn(
+                        aResponse().withStatus(HttpStatus.OK.value())));
+    }
+
+    @Test
+    public void contextLoads() {
+        webClient
+                .get()
+                .uri("/api/appointments")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    public void contextFails() {
+        webClient
+                .get()
+                .uri("/api/users")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
---
+++ api-gateway/src/test/java/com/com3014/apigateway/SpringGatewayUserAuthRoutingTest.java
@@ -0,0 +1,57 @@
+package com.com3014.apigateway;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWireMock(port = 8082)
+public class SpringGatewayUserAuthRoutingTest {
+
+    @LocalServerPort
+    protected int port = 0;
+
+    protected WebTestClient webClient;
+
+    @BeforeEach
+    public void setup() {
+        this.webClient = WebTestClient
+                .bindToServer()
+                .responseTimeout(Duration.ofSeconds(10))
+                .baseUrl("http://localhost:" + port)
+                .build();
+
+        stubFor(
+                get(urlMatching("/api/users.*")).willReturn(
+                        aResponse().withStatus(HttpStatus.OK.value())));
+    }
+
+    @Test
+    public void contextLoads() {
+        webClient
+                .get()
+                .uri("/api/users")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    public void contextFails() {
+        webClient
+                .get()
+                .uri("/api/appointment")
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+}
