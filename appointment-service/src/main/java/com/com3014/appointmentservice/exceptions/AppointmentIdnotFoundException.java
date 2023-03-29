e14a03c06557f22d5adda76e5e42bfa739a16127
Jeniffer Maria <jeniffermaria@Jeniffers-MacBook-Pro.local>
2023-03-29 21:01:00 +0100

    revert

--- appointment-service/src/main/java/com/com3014/appointmentservice/exceptions/AppointmentIdnotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.com3014.appointmentservice.exceptions;
-
-public class AppointmentIdnotFoundException extends RuntimeException {
-    public AppointmentIdnotFoundException(String errorMessage) {
-        super(errorMessage);
-    }
-}
