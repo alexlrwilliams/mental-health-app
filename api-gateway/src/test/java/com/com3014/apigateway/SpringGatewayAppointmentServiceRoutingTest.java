35931cfe7fc55368c95010bb4134f7658505c09d
Alex Williams <aw01121@surrey.ac.uk>
2023-03-30 17:21:48 +0100

    MHC-17: Create API Gateway (#8)
    * MHC-17: Changed microservice ports
    
    * MHC-17: Create initial API Gateway and routes
    
    * MHC-17: Addded route tests
    
    * MHC-17: Added api gateway to github actions pipeline, updated tests
    
    * MHC-17: Addded execution permission to gradle wrapper
    
    * MHC-17: added application-test.properties and updated test names

---
+++ api-gateway/src/test/java/com/com3014/apigateway/SpringGatewayAppointmentServiceRoutingTest.java
@@ -0,0 +1,60 @@
+package com.com3014.apigateway;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWireMock(port = 5002)
+@ActiveProfiles("test")
+public class SpringGatewayAppointmentServiceRoutingTest {
+
+    @LocalServerPort
+    protected int port = 0;
+
+    protected WebTestClient webClient;
+
+    @BeforeEach
+    public void setup() {
+        this.webClient = WebTestClient
+                .bindToServer()
+                .responseTimeout(Duration.ofSeconds(10))
+                .baseUrl("http://localhost:" + port)
+                .build();
+
+        stubFor(
+                get(urlMatching("/api/appointments.*")).willReturn(
+                        aResponse().withStatus(HttpStatus.OK.value())));
+    }
+
+    @Test
+    public void appointment_route__successful() {
+        webClient
+                .get()
+                .uri("/api/appointments")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    public void other_route__unsuccessful() {
+        webClient
+                .get()
+                .uri("/api/users/test")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
