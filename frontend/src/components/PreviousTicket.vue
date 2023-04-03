5855c7b4d3ef9f461781e2054a2d63ee66e4d811
Ahmed Henine <ah01567@gmail.com>
2023-04-03 11:58:41 +0100

    Added previous appointment ticket to home

---
+++ frontend/src/components/PreviousTicket.vue
@@ -0,0 +1,8 @@
+<template>
+    <b-card bg-variant="light" title="Monday, April the 3rd, 2023 - 09:15pm">
+    <b-card-text>
+       Your appointment with Dr. <b>Alex Williams</b> has terminated. You are able to recheck your chat with the doctor by clicking the button below:. 
+    </b-card-text>
+    <b-button class="chat-access" variant="primary">Access chat</b-button>
+    </b-card> 
+</template>
