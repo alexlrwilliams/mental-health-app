5b2a1fbddf5f4d6b7811e06d6c0d94489c9676dd
Ahmed Henine <ah01567@gmail.com>
2023-04-09 00:23:16 +0100

    wrapped the three tickets in one b-card

--- frontend/src/components/AppointmentTicket.vue
+++ frontend/src/components/AppointmentTicket.vue
@@ -1,33 +1,29 @@
 <template>
   <div>
+    <b-card class="ticket" bg-variant="light" :title="formattedDate">
+        <template v-if="appointment.status === 'upcoming'">
+                <b-card-text>
+                    Your appointment with Dr. <b>{{ appointment.doctor }}</b> is now confirmed. You will be able to join the appointment 10min before the booked timing. 
+                </b-card-text>
+                <b-button class="join-btn" variant="success">Join</b-button>                
+                <b-button class="cancel-btn" variant="danger">Cancel</b-button>
+        </template>
 
-    <template v-if="appointment.status === 'upcoming'">
-        <b-card class="ticket" bg-variant="light" :title="appointment.date">
-            <b-card-text>
-                Your appointment with Dr. <b>{{ appointment.doctor }}</b> is now confirmed. You will be able to join the appointment 10min before the booked timing. 
-            </b-card-text>
-            <b-button class="join-btn" variant="success">Join</b-button>                
-            <b-button class="cancel-btn" variant="danger">Cancel</b-button>
-        </b-card> 
-    </template>
+        <template v-if="appointment.status === 'previous'">
 
-    <template v-if="appointment.status === 'previous'">
-        <b-card class="ticket"  bg-variant="light" :title="appointment.date">
-            <b-card-text>
-            Your appointment with Dr. <b>{{ appointment.doctor }}</b> has terminated. However, you are still able to access your previous chat with the docor, by clicking the button below. 
-            </b-card-text>
-            <b-button class="chat-access" variant="primary">Access chat</b-button>
-        </b-card> 
-    </template>
+                <b-card-text>
+                Your appointment with Dr. <b>{{ appointment.doctor }}</b> has terminated. However, you are still able to access your previous chat with the docor, by clicking the button below. 
+                </b-card-text>
+                <b-button class="chat-access" variant="primary">Access chat</b-button>
+        </template>
 
-    <template v-if="appointment.status === 'cancelled'">
-        <b-card class="ticket"  bg-variant="light" :title="appointment.date">
-            <b-card-text>
-            Your appointment with Dr. <b>{{ appointment.doctor }}</b> is cancelled. If you wish to book another appointment, please refer to the green button at the top of the page. 
-            </b-card-text>
-        </b-card> 
-    </template>
+        <template v-if="appointment.status === 'cancelled'">
 
+                <b-card-text>
+                Your appointment with Dr. <b>{{ appointment.doctor }}</b> is cancelled. If you wish to book another appointment, please refer to the green button at the top of the page. 
+                </b-card-text>
+        </template>
+      </b-card>
   </div>
 </template>
 
@@ -41,6 +37,7 @@ export default {
       required: true
     }
   }
+  
 }
 </script>
 
