740d84c203e89d52f80ba5213cb584ca6b085276
Ahmed Henine <ah01567@gmail.com>
2023-04-09 15:19:10 +0100

    Removed the status as an appointment condition.

--- frontend/src/components/AppointmentTicket.vue
+++ frontend/src/components/AppointmentTicket.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
     <b-card class="ticket" bg-variant="light" :title="formattedDate">
-        <template v-if="appointment.status === 'upcoming'">
-                <b-card-text>
-                    Your appointment with Dr. <b>{{ appointment.doctor }}</b> is now confirmed. You will be able to join the appointment 10min before the booked timing. 
-                </b-card-text>
-                <b-button class="join-btn" variant="success">Join</b-button>                
-                <b-button class="cancel-btn" variant="danger">Cancel</b-button>
+        <template v-if="appointment.cancelled">
+            <b-card-text>
+                Your appointment with Dr. <b>{{ appointment.docId }}</b> is cancelled. If you wish to book another appointment, please refer to the green button at the top of the page. 
+            </b-card-text>
         </template>
 
-        <template v-if="appointment.status === 'previous'">
-
-                <b-card-text>
-                Your appointment with Dr. <b>{{ appointment.doctor }}</b> has terminated. However, you are still able to access your previous chat with the docor, by clicking the button below. 
-                </b-card-text>
-                <b-button class="chat-access" variant="primary">Access chat</b-button>
+        <template v-else-if="isAppointmentPrevious">
+          <b-card-text>
+            Your appointment with Dr. <b>{{ appointment.docId }}</b> has terminated.
+            {{ appointment.type === 'CHAT' ? ' However, you are still able to access your previous chat with the doctor, by clicking the button below.' : '' }}
+          </b-card-text>
+          <b-button v-if="appointment.type === 'CHAT'" class="chat-access" variant="primary">Access chat</b-button>
         </template>
 
-        <template v-if="appointment.status === 'cancelled'">
-
-                <b-card-text>
-                Your appointment with Dr. <b>{{ appointment.doctor }}</b> is cancelled. If you wish to book another appointment, please refer to the green button at the top of the page. 
-                </b-card-text>
+        <template v-else>
+            <b-card-text>
+                Your appointment with Dr. <b>{{ appointment.docId }}</b> is now confirmed. You will be able to join the appointment 10min before the booked timing. 
+            </b-card-text>
+            <b-button class="join-btn" variant="success">Join</b-button>                
+            <b-button class="cancel-btn" variant="danger">Cancel</b-button>
         </template>
       </b-card>
   </div>
@@ -36,8 +35,22 @@ export default {
       type: Object,
       required: true
     }
+  },
+    computed: {
+    formattedDate() {
+      // Format date and time to display on the ticket
+      const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+      const start = new Date(this.appointment.startTime).toLocaleDateString(undefined, options);
+      const end = new Date(this.appointment.endTime).toLocaleTimeString(undefined, options);
+      return `${start} - ${end}`;
+    },
+    isAppointmentPrevious() {
+      // Check if the appointment has ended
+      const now = new Date();
+      const endTime = new Date(this.appointment.endTime);
+      return endTime < now;
+    }
   }
-  
 }
 </script>
 
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -40,40 +40,103 @@ export default {
   components: {
     AppointmentTicket,
   },
-  data() {
-    return {
-      upcomingAppointments: [
-        {
-          date: "Wednesday, April the 11th, 2023 - 03:15pm",
-          doctor: "Ahmed Henine",
-          status: "upcoming",
+data() {
+  return {
+    appointments: [
+{
+            id: "68cdefc7-669c-48be-a148-6506dc2f3637",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-04-21T15:30:00.552842800Z",
+            endTime: "2023-04-21T15:45:00.552842800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "my mental health be rough lol",
+            type: "VIDEO",
+            cancelled: false
         },
         {
-          date: "Friday, April the 7th, 2023 - 09:00am",
-          doctor: "Alex Williams",
-          status: "upcoming",
-        }
-      ], 
-      previousAppointments: [
-        {
-          date: "Tuesday, March the 21st, 2023 - 04:15pm",
-          doctor: "Jenny Maria",
-          status: "previous",
+            id: "347c3001-5112-42cf-8576-1d075c44be52",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-05-06T20:30:00.552842800Z",
+            endTime: "2023-05-06T20:45:00.552842800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "probs like anxiety or depression lol",
+            type: "VIDEO",
+            cancelled: false
         },
         {
-          date: "Monday, March the 13th, 2023 - 012:30pm",
-          doctor: "Karthi",
-          status: "previous",
-        }
-      ], 
-      cancelledAppointments: [
+            id: "0228e247-b83f-4d9c-86cf-308990f0c463",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-04-19T14:15:00.552842800Z",
+            endTime: "2023-04-19T14:30:00.552842800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "anxiety maybe? who knows lol",
+            type: "VIDEO",
+            cancelled: false
+        },
         {
-          date: "Friday, April the 14th, 2023 - 10:45am",
-          doctor: "Al-Ayhem",
-          status: "cancelled",
-        } 
-      ]
-  }},
+            id: "428bbd7c-e7fc-4050-bf52-6fd3bfe04d44",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-05-07T19:30:00.552842800Z",
+            endTime: "2023-05-07T19:45:00.552842800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "recently had depression or something",
+            type: "VIDEO",
+            cancelled: false
+        },
+        {
+            id: "decfaf62-dfac-45fd-9638-b9f2723c930b",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-08-07T16:30:00.609940900Z",
+            endTime: "2023-08-07T17:45:00.609940900Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "test123",
+            type: "CHAT",
+            cancelled: false
+        },
+        {
+            id: "36c5c9b8-fea4-4f1c-ae92-2e06b638aa86",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-04-06T20:00:00.552842800Z",
+            endTime: "2023-04-06T20:15:00.552842800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "depression",
+            type: "VIDEO",
+            cancelled: false
+        },
+        {
+            id: "a848bd57-3b42-4ec4-884f-29b55a2c7128",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-04-06T12:15:00.739546500Z",
+            endTime: "2023-04-06T12:30:00.739546500Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "anxiety",
+            type: "CHAT",
+            cancelled: false
+        },
+        {
+            id: "b484c8b5-eb29-4013-ac66-eec7b77f65b4",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-05-08T19:30:00.300796800Z",
+            endTime: "2023-05-08T19:45:00.300796800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "summary",
+            type: "CHAT",
+            cancelled: true
+        },
+        {
+            id: "7889a5a0-5586-4aba-bde0-abb11d660de9",
+            docId: "0e5c9e0e-65a1-48c1-a8b3-84fe12204055",
+            startTime: "2023-03-01T09:00:00.552842800Z",
+            endTime: "2023-03-01T09:15:00.552842800Z",
+            patientId: "552c9a7a-202a-46bf-865a-6147d32bf165",
+            summary: "depression",
+            type: "VIDEO",
+            cancelled: true
+        }
+    ]
+  }
+}
+
 }
 </script>
 
