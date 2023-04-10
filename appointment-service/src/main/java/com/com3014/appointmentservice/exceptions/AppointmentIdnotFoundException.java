4b6f7157c71475c94c5040f9335651389d5ce1fe
Jeniffer Maria <jeniffermaria@Jeniffers-MacBook-Pro.local>
2023-04-10 07:17:20 +0100

    delete

--- appointment-service/src/main/java/com/com3014/appointmentservice/exceptions/AppointmentIdnotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.com3014.appointmentservice.exceptions;
-
-public class AppointmentIdNotFoundException extends RuntimeException {
-    public AppointmentIdNotFoundException(String errorMessage) {
-        super(errorMessage);
-    }
-}
