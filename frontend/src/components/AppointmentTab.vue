90b6b744d5a562587f3363fb5deb9ac086ec821e
Alex Williams <aw01121@surrey.ac.uk>
2023-04-12 18:55:13 +0100

    MHC-47: Added initial tabs to home page

---
+++ frontend/src/components/AppointmentTab.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-tab :title="title" :active="active">
+    <b-card class="appointment-box">
+      <b-card-header>
+        <h4 :class="headerClass"> {{ title }} Appointments <span :class="dotClass"></span> </h4>
+      </b-card-header>
+      <b-card-body>
+        <AppointmentTicket v-for="appointment in appointments" :key="appointment.id" :appointment="appointment" />
+      </b-card-body>
+    </b-card>
+  </b-tab>
+</template>
+
+<script>
+import AppointmentTicket from "@/components/AppointmentTicket.vue";
+
+export default {
+  name: "AppointmentTab",
+  components: {
+    AppointmentTicket
+  },
+  props: {
+    appointments: {type: Array, required: true},
+    title: {type: String, required: true},
+    dotClass: {type: String, default: ""},
+    active: {type: Boolean, default: false},
+    headerClass: {type: String, default: ""}
+  }
+}
+</script>
+
+<style scoped>
+  .appointment-box {
+    width: calc(100% - 10px);
+    margin: 0 auto;
+  }
+  .upcoming-dot {
+    height: 13px;
+    width: 13px;
+    background-color: green;
+    border-radius: 50%;
+    display: inline-block;
+  }
+  .previous-dot {
+    height: 13px;
+    width: 13px;
+    background-color: rgb(192, 15, 15);
+    border-radius: 50%;
+    display: inline-block;
+  }
+  .cancelled-title {
+    color: brown;
+  }
+</style>
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -2,43 +2,21 @@
   <div>
     <div class='col-md-8 mx-auto center'>
       <b-button class="book-appointment-btn" variant='success' to='/book-appointment' ><h4>Book an appointment</h4></b-button>
-      <b-card class="appointment-box">
-              <b-card-header>
-                <h4>Upcoming appointments <span class="upcoming-dot"></span></h4>
-              </b-card-header>
-              <b-card-body>
-                  <AppointmentTicket v-for="(appointment, index) in upcomingAppointments" :key="index" :appointment="appointment" />
-              </b-card-body>
-      </b-card>
-
-      <b-card class="appointment-box">
-              <b-card-header>
-                <h4>Previous appointments <span class="previous-dot"></span></h4>
-              </b-card-header>
-              <b-card-body>
-                <AppointmentTicket v-for="(appointment, index) in previousAppointments" :key="index" :appointment="appointment" />
-              </b-card-body>
-      </b-card>
-
-      <b-card class="appointment-box">
-              <b-card-header>
-                <h4 class="cancelled-title">Cancelled appointments:</h4>
-              </b-card-header>
-              <b-card-body>
-                <AppointmentTicket v-for="(appointment, index) in cancelledAppointments" :key="index" :appointment="appointment"/>
-              </b-card-body>
-      </b-card>
-
+      <b-tabs content-class="mt-3">
+        <AppointmentTab title="Upcoming" :appointments="upcomingAppointments" dot-class="upcoming-dot" :active="true"/>
+        <AppointmentTab title="Previous" :appointments="previousAppointments" dot-class="previous-dot"/>
+        <AppointmentTab title="Cancelled" :appointments="cancelledAppointments" header-class="cancelled-title"/>
+      </b-tabs>
     </div>
   </div>
 </template>
 
 <script>
-import AppointmentTicket from '../components/AppointmentTicket.vue';
+import AppointmentTab from "@/components/AppointmentTab.vue";
 export default {
   name: 'HomePage', 
   components: {
-    AppointmentTicket,
+    AppointmentTab,
   },
 data() {
   return {
@@ -174,26 +152,5 @@ computed: {
   }
   .book-appointment-btn {
     width: 80%;
-  }
-  .appointment-box {
-    width: calc(100% - 10px);
-    margin: 0 auto;
-  }
-  .upcoming-dot {
-    height: 13px;
-    width: 13px;
-    background-color: green;
-    border-radius: 50%;
-    display: inline-block;
-  }
-  .previous-dot {
-    height: 13px;
-    width: 13px;
-    background-color: rgb(192, 15, 15);
-    border-radius: 50%;
-    display: inline-block;
-  }
-  .cancelled-title {
-    color: brown;
   }
 </style>
