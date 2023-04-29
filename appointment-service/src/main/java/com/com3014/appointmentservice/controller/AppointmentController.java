d431df895616615a38b0682b6b39b8bb844e6187
Alex Williams <aw01121@surrey.ac.uk>
2023-04-29 21:10:06 +0100

    MHC-30: Changed update method to call update in service

--- appointment-service/src/main/java/com/com3014/appointmentservice/controller/AppointmentController.java
+++ appointment-service/src/main/java/com/com3014/appointmentservice/controller/AppointmentController.java
@@ -3,12 +3,13 @@ package com.com3014.appointmentservice.controller;
 import com.com3014.appointmentservice.model.Appointment;
 import com.com3014.appointmentservice.model.json.AppointmentJson;
 import com.com3014.appointmentservice.service.AppointmentService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.*;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-import jakarta.validation.Valid;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
@@ -62,11 +63,6 @@ public class AppointmentController {
     }
 
     @PutMapping("/{id}")
-    public Appointment updateAppointment(@PathVariable UUID id,@RequestBody AppointmentJson json){
-        return appointmentService.updateAppointment(id,json);
-    }
-
-    @PutMapping
     public ResponseEntity<?> updateAppointment(@PathVariable UUID id, @Valid @RequestBody AppointmentJson json, BindingResult result) {
         if (result.hasErrors()) {
             List<String> errors = result.getAllErrors()
@@ -83,7 +79,7 @@ public class AppointmentController {
 
             return ResponseEntity.badRequest().body(errors);
         } else {
-            Appointment appointment = appointmentService.createAppointment(json);
+            Appointment appointment = appointmentService.updateAppointment(id,json);
             return ResponseEntity.ok(appointment);
         }
     }
