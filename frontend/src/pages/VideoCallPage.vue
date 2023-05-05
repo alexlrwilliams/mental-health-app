d6442e7e9a23a9636882d1db6c0a882d8934a6d1
Ahmed Henine <ah01567@gmail.com>
2023-05-05 18:39:33 +0100

    Video calling with roomId added

--- frontend/src/pages/VideoCallPage.vue
+++ frontend/src/pages/VideoCallPage.vue
@@ -1,50 +1,168 @@
 <template>
     <div class="container">
-        <div class="video">
-            <video id="localVideo" autoplay></video>
-            <video id="remoteVideo" autoplay></video>
+            <div class="video-list">
+        <div v-for="item in videoList"
+             :key="item.id"
+             class="video-item">
+            <video autoplay playsinline ref="videos"  :muted="item.muted" :id="item.id">
+              <p>User has left </p>
+            </video>
         </div>
+    </div>
+
         <div class="buttons">
-            <b-button class="join-video-btn" variant='success' @click="join">Join</b-button>
-            <b-button class="leave-video-btn" variant='danger' @click="leave">Leave</b-button>
+            <b-button class="leave-video-btn" variant='danger' @click="leave" to="/" >Leave</b-button>
         </div>
 
-        <VueWebRTC
-            ref="webrtc"
-            :auto="false"
-            :local-media-options="{ video: true, audio: true }"
-            :remote-media-options="{ video: true, audio: true }"
-            :ice-servers="[{ urls: 'stun:stun.l.google.com:19302' }]"
-            @local-media-stream="onLocalMediaStream"
-            @remote-media-stream="onRemoteMediaStream"
-        />
 
     </div>
 </template>
 
 <script>
-//import VueWebRTC from 'vue-webrtc'
-export default {
+    import { defineComponent } from 'vue';
+    import { io } from "socket.io-client";
+    const SimpleSignalClient = require('simple-signal-client');
+    export default /*#__PURE__*/defineComponent({
+        name: 'vue-webrtc',
+        components: {
+        },
+        data() {
+            return {
+                signalClient: null,
+                videoList: [],
+                canvas: null,
+                socket: null,
+                socketURL: 'https://weston-vue-webrtc-lobby.azurewebsites.net'
+            };
+        },
+        props: {
+            roomId: {
+                type: String,
+                default: 'public-room-v2'
+            },
+            peerOptions: {
+                type: Object,  // NOTE: use these options: https://github.com/feross/simple-peer
+                default() {
+                    return {};
+                }
+            },
+            ioOptions: {
+                type: Object,  // NOTE: use these options: https://socket.io/docs/v4/client-options/
+                default() {
+                    return { rejectUnauthorized: false, transports: ['polling', 'websocket'] };
+                }
+            },
+            deviceId: {
+                type: String,
+                default: null
+            }
+        },
+        mounted() {
+          this.join();
+        },
+        beforeDestroy() {
+          this.leave();
+        },
         methods: {
-        join() {
-            this.$refs.webrtc.join()
-        },
-        leave() {
-            this.$refs.webrtc.leave()
-        },
-        onLocalMediaStream(stream) {
-            const localVideo = document.getElementById('localVideo')
-            localVideo.srcObject = stream
-        },
-        onRemoteMediaStream(stream) {
-            const remoteVideo = document.getElementById('remoteVideo')
-            remoteVideo.srcObject = stream
+            async join() {
+                var that = this;
+                this.socket = io(this.socketURL, this.ioOptions);
+                this.signalClient = new SimpleSignalClient(this.socket);
+                let constraints = {
+                    video: true,
+                    audio: true
+                };
+                if (that.deviceId) {
+                    constraints.video = { deviceId: { exact: that.deviceId } };
+                }
+                const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+                this.joinedRoom(localStream, true);
+                this.signalClient.once('discover', (discoveryData) => {
+                    async function connectToPeer(peerID) {
+                        if (peerID == that.socket.id) return;
+                        try {
+                            const { peer } = await that.signalClient.connect(peerID, that.roomId, that.peerOptions);
+                            that.videoList.forEach(v => {
+                                if (v.isLocal) {
+                                    that.onPeer(peer, v.stream);
+                                }
+                            })
+                        } catch (e) {
+                          console.log(e);
+                        }
+                    }
+                    discoveryData.peers.forEach((peerID) => connectToPeer(peerID));
+                    that.$emit('opened-room', that.roomId);
+                });
+                this.signalClient.on('request', async (request) => {
+                    const { peer } = await request.accept({}, that.peerOptions)
+                    that.videoList.forEach(v => {
+                        if (v.isLocal) {
+                            that.onPeer(peer, v.stream);
+                        }
+                    })
+                })
+                this.signalClient.discover(that.roomId);
+            },
+            onPeer(peer, localStream) {
+                var that = this;
+                peer.addStream(localStream);
+                peer.on('stream', (remoteStream) => {
+                    that.joinedRoom(remoteStream, false);
+                    peer.on('close', () => {
+                        var newList = [];
+                        that.videoList.forEach(function (item) {
+                            if (item.id !== remoteStream.id) {
+                                newList.push(item);
+                            }
+                        });
+                        that.videoList = newList;
+                        that.$emit('left-room', remoteStream.id);
+                    });
+                    peer.on('error', (err) => {
+                        console.log(err);
+                    });
+                });
+            },
+            joinedRoom(stream, isLocal) {
+                var that = this;
+                let found = that.videoList.find(video => {
+                    return video.id === stream.id
+                })
+                if (found === undefined) {
+                    let video = {
+                        id: stream.id,
+                        muted: isLocal,
+                        stream: stream,
+                        isLocal: isLocal
+                    };
+                    that.videoList.push(video);
+                }
+                setTimeout(function () {
+                    for (var i = 0, len = that.$refs.videos.length; i < len; i++) {
+                        if (that.$refs.videos[i].id === stream.id) {
+                            that.$refs.videos[i].srcObject = stream;
+                            break;
+                        }
+                    }
+                }, 500);
+                that.$emit('joined-room', stream.id);
+            },
+            leave() {
+                this.videoList.forEach(v => v.stream.getTracks().forEach(t => t.stop()));
+                this.videoList = [];
+                this.signalClient.peers().forEach(peer => peer.removeAllListeners())
+                this.signalClient.destroy();
+                this.signalClient = null;
+                this.socket.destroy();
+                this.socket = null;
+            },
+            }
         }
-      }
-}
+    );
 </script>
 
-<style>
+<style scoped>
 .container {
   display: flex;
   align-items: center;
@@ -65,4 +183,19 @@ export default {
   margin-left: 5px;
   width: 150px;
 }
+    .video-list {
+        background: whitesmoke;
+        height: auto;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+        .video-list div {
+            padding: 0px;
+        }
+    .video-item {
+        background: #c5c4c4;
+        display: inline-block;
+    }
 </style>
